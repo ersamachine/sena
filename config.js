4da622ae89783844badb9d7b88a911e9688b9e58
ersamachine <easyboxmachine@gmail.com>
2026-07-01 16:15:40 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// SENA Gümrük · Apps Script web app adresi.
+// Sena Apps Script'ini "Dağıt → Web app" yapınca çıkan /exec adresini tırnak içine yapıştır:
+window.SENA_API_URL = "BURAYA_SENA_APPS_SCRIPT_EXEC_ADRESI";
